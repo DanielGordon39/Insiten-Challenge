f610864dd5dab031dcd18acfb5f2340f7397cc9a
unknown <dgordon45@gatech.edu>
2020-01-18 16:47:08 -0500

    more organized

--- src/insiten/ParseJSON.java
+++ src/insiten/ParseJSON.java
@@ -1,17 +1,35 @@
 package insiten;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class ParseJSON {
 
-    public static LinkedList<Company> readJSON(String filename) throws Exception {
+    public static LinkedList<Company> readJSON(String filename) throws FileNotFoundException {
+        LinkedList<Company> list = new LinkedList<>();
+        String curr;
         Scanner sc = new Scanner(new File(filename));
         while (sc.hasNextLine()) {
-            System.out.println(sc.nextLine());
+            if (sc.nextLine().contains("{")) {
+                Company company = new Company();
+                curr = sc.nextLine();
+                curr = curr.substring(11, curr.length() - 2);
+                company.setName(curr);
+                curr = sc.nextLine();
+                curr = curr.substring(13, curr.length() - 2);
+                company.setStatus(curr);
+                curr = sc.nextLine();
+                curr = curr.substring(14, curr.length() - 1);
+                company.setRevenue(Long.parseLong(curr));
+                curr = sc.nextLine();
+                curr = curr.substring(15, curr.length() - 1);
+                company.setLocation(curr);
+                list.add(company);
+            }
         }
-        return null;
+        return list;
     }
 
 }
